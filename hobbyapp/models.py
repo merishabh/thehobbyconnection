480053a7a108a597f66999981daa12a140058649
merishabh <rishabh.saxena2112@gmail.com>
2016-08-19 15:02:50 +0530

    updated User model

--- hobbyapp/models.py
+++ hobbyapp/models.py
@@ -6,11 +6,19 @@ from djutil.models import TimeStampedModel
 from django.contrib.auth.models import AbstractBaseUser, User
 
 class User(AbstractBaseUser, TimeStampedModel):
+    first_name = models.CharField(_('first name'), max_length=254, blank=True, default="")
+    middle_name = models.CharField(_('middle name'), max_length=254, blank=True, default="")
+    last_name = models.CharField(_('last_name name'), max_length=254, blank=True, default="")
+    gender = models.CharField(_('gender'), max_length=10, blank=True, default="")
+    image = models.CharField(_('image'), max_length=1000, blank=True, default="")
+    hometown = models.CharField(_('hometown'), max_length=254, blank=True, default="")
     email = models.EmailField(_('email address'), max_length=254, unique=True, primary_key=True)
-    access_token = models.CharField(max_length=150)
+    access_token = models.CharField(max_length=15000)
     facebook_id = models.BigIntegerField()
     USERNAME_FIELD = 'email'
 
+    class Meta:
+        ordering = ['-modified_at',]
 
 class HobbyGroup(TimeStampedModel):
     hobby_id = models.AutoField(primary_key=True)
